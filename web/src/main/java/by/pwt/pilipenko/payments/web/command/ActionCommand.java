d05b69926d26f8c971dcaa10aed1277a3183eac8
apilipenka <pilipenko@gmail.com>
2016-09-09 20:55:56 +0300

    last before spring

--- web/src/main/java/by/pwt/pilipenko/payments/web/command/ActionCommand.java
+++ web/src/main/java/by/pwt/pilipenko/payments/web/command/ActionCommand.java
@@ -4,5 +4,11 @@ package by.pwt.pilipenko.payments.web.command;
 import javax.servlet.http.HttpServletRequest;
 
 public interface ActionCommand {
+    /**
+     *
+     * @param request
+     * @return
+     * @throws Exception
+     */
     String execute(HttpServletRequest request) throws Exception;
 }
